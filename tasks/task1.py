e1919ba4ab5cdd3ee3ed5f55fcad7a96684e5edd
flleonte <flleonte@hp_wsl>
2026-08-20 00:33:38 +0200

    task1 clean

--- tasks/task1.py
+++ tasks/task1.py
@@ -15,20 +15,10 @@ case = (
     .shell(-2)
     .edges("|Z")
     .fillet(case_fillet_r)
-#    .edges("|<X")
     .faces("<Z")
     .fillet(0.35)
     .workplane(origin=(-13, 59.5, 0))
     .hole(hole_diameter)
-#    .workplane(origin=(21, 55, -5))
-#    .box(12, 17, 10)
-#    .edges("|Z")
-#    .fillet(1.5)
-#    .workplane(origin=(0, -55, -5))
-#    .box(20, 6, 10)
-#    .faces("+X")
-#    .edges(">Z")
-#    .fillet(0.75)
 )
 
 #bool cuts in Z axis
@@ -51,15 +41,10 @@ bool_cut_Z2 = (
 bool_cut_Y1 = (
     cq.Workplane(origin=(9.5, -70, 0))
     .box(9, 10, 5)
-#    .cylinder(10, 1, (0, 10, 2.675))
-#    .workplane(origin=(25, 2.675, 0))
-#    .hole(2)
 )
 bool_cut_Y2 = (
     cq.Workplane(origin=(25, -70, 2.675))
     .cylinder(10, 1, (0, 1, 0))
-#    .workplane(origin=(-22, 70, 0))
-#    .cylinder(10, 3.5, (0, 1, 0))
 )
 bool_cut_Y3 = (
     cq.Workplane(origin=(-22, 70, 0.775))
