ec9e0471f8c2ffe825bb21606d09f6c019b09031
flleonte <flleonte>
2026-08-19 19:41:09 +0200

    testing on task1

--- tasks/task1.py
+++ tasks/task1.py
@@ -3,8 +3,12 @@ import cadquery as cq
 w = 14
 h = 6.9
 t = 1
+case_fillet_r = w * 0.1
 
-case = cq.Workplane("XY", [0,0,0]).box(h, w, t)
+case = cq.Workplane("XY", [0,0,0]).box(h, w, t)#.faces("Z").edges().fillet(1)
+
+s = cq.Workplane("XY", [0,0,0]).box(5, 5, 5).faces("Z").edges().fillet(0.1)
 
 
 show_object(case)
+show_object(s)
