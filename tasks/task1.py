4487be45fd442b71fdffe77279975eac20527c08
flleonte <flleonte@hp_wsl>
2026-08-20 00:32:53 +0200

    added launcher and task1 works

--- tasks/task1.py
+++ tasks/task1.py
@@ -1,14 +1,74 @@
 import cadquery as cq
 
-w = 14
-h = 6.9
-t = 1
-case_fillet_r = w * 0.1
+w = 140
+h = 69
+t = 10
+case_fillet_r = 10
+hole_diameter = 7
 
-case = cq.Workplane("XY", [0,0,0]).box(h, w, t)#.faces("Z").edges().fillet(1)
+#base generation
 
-s = cq.Workplane("XY", [0,0,0]).box(5, 5, 5).faces("Z").edges().fillet(0.1)
+case = (
+    cq.Workplane("XY", [0,0,0])
+    .box(h, w, t)
+    .faces("+Z")
+    .shell(-2)
+    .edges("|Z")
+    .fillet(case_fillet_r)
+#    .edges("|<X")
+    .faces("<Z")
+    .fillet(0.35)
+    .workplane(origin=(-13, 59.5, 0))
+    .hole(hole_diameter)
+#    .workplane(origin=(21, 55, -5))
+#    .box(12, 17, 10)
+#    .edges("|Z")
+#    .fillet(1.5)
+#    .workplane(origin=(0, -55, -5))
+#    .box(20, 6, 10)
+#    .faces("+X")
+#    .edges(">Z")
+#    .fillet(0.75)
+)
 
+#bool cuts in Z axis
+
+bool_cut_Z1 = (
+    cq.Workplane(origin=(21, 55, -5))
+    .box(12, 17, 10)
+    .edges("|Z")
+    .fillet(1.5)
+)
+bool_cut_Z2 = (
+    cq.Workplane(origin=(0, -55, -5))
+    .box(20, 6, 10)
+    .edges("<X")
+    .fillet(0.75)
+)
+
+#bool cuts in Y axis
+
+bool_cut_Y1 = (
+    cq.Workplane(origin=(9.5, -70, 0))
+    .box(9, 10, 5)
+#    .cylinder(10, 1, (0, 10, 2.675))
+#    .workplane(origin=(25, 2.675, 0))
+#    .hole(2)
+)
+bool_cut_Y2 = (
+    cq.Workplane(origin=(25, -70, 2.675))
+    .cylinder(10, 1, (0, 1, 0))
+#    .workplane(origin=(-22, 70, 0))
+#    .cylinder(10, 3.5, (0, 1, 0))
+)
+bool_cut_Y3 = (
+    cq.Workplane(origin=(-22, 70, 0.775))
+    .cylinder(10, 3.5, (0, 1, 0))
+)
+case = case.cut(bool_cut_Z1)
+case = case.cut(bool_cut_Z2)
+case = case.cut(bool_cut_Y1)
+case = case.cut(bool_cut_Y2)
+case = case.cut(bool_cut_Y3)
 
 show_object(case)
-show_object(s)
